39929e9aed6c0984b364af1af9b8b92421090355
Wesley Coakley <w@wesleycoakley.com>
2022-07-24 05:09:47 -0400

    passmenu

--- rc.lua
+++ rc.lua
@@ -212,6 +212,8 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey, "Shift"   }, "p", function () awful.spawn("passmenu") end,
+              {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
